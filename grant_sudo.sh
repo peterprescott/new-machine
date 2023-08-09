2cf1611153c1b77ca9b889c6cbd78c6e84fb0be4
Peter Prescott <prescott.peter@gmail.com>
2023-08-09 09:54:11 +0100

    Add script for root to grant sudo rights

---
+++ grant_sudo.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# Prompt for the username
+read -p "Who needs sudo rights? " username
+
+# Check if the user exists
+if id "$username"; then
+    # Add the user to the sudo group
+    sudo usermod -aG sudo "$username"
+    echo "Sudo rights granted to user $username."
+else
+    echo "User $username does not exist."
+fi

5242532c52240d74574ab2aff287ac87024022f5
Peter Prescott <prescott.peter@gmail.com>
2023-08-09 20:45:03 +0100

    Remove renamed scripts

--- scripts/05-update-grub.sh
+++
@@ -1,9 +0,0 @@
-#######################################
-# Update Grub
-#######################################
-
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-sudo update-grub
-
-#######################################
-

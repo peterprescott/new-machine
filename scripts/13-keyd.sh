67bce86b1c71b4c582d1217316adb509406ec24a
Peter Prescott <prescott.peter@gmail.com>
2025-01-16 14:30:48 +0000

    Add keyd config file to repo

--- scripts/13-keyd.sh
+++ scripts/13-keyd.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/rvaiya/keyd
 cd keyd
 make && sudo make install
+sudo 
 sudo systemctl enable keyd && sudo systemctl start keyd
-cd ..
+sudo cp /home/peterprescott/new-machine/keyd_config/default.conf /etc/keyd/default.conf
 rm -rf keyd
